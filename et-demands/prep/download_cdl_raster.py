f90499f899fa4883b8e15de8e8c462b87d359e8c
Charles Morton <cgmorton@users.noreply.github.com>
2019-01-09 07:49:42 -0800

    Switched CDL downloader to use FTP module for download
    The urllib and urllib2 modules might be a little better but until we write this to be fully Python 2/3 compatible (which is kind of annoying for urllib), using the FTP module is really easy. The CDL data in general is a pain since it is hosted on a real FTP server (so we can't use requests) and it seems like the download speed is really throttled.

--- et-demands/prep/download_cdl_raster.py
+++ et-demands/prep/download_cdl_raster.py
@@ -8,8 +8,6 @@ import datetime as dt
 import logging
 import os
 import sys
-import time
-import urllib
 import zipfile
 
 import _util as util
@@ -33,9 +31,10 @@ def main(ini_path, overwrite_flag=False):
     logging.info('\nDownload and extract CONUS CDL rasters')
 
     logging.debug('INI: {}'.format(ini_path))
-    config = util.read_ini(ini_path)
+    config = util.read_ini(ini_path, section='CROP_ET')
 
-    site_url = 'ftp://ftp.nass.usda.gov/download/res'
+    site_url = 'ftp.nass.usda.gov'
+    site_folder = 'download/res'
 
     cdl_ws = config.get('CROP_ET', 'cdl_folder')
     cdl_year = int(config.get('CROP_ET', 'cdl_year'))
@@ -48,35 +47,35 @@ def main(ini_path, overwrite_flag=False):
 
     cdl_path = os.path.join(cdl_ws, cdl_format.format(cdl_year, 'img'))
 
-    zip_url_size = remote_size(zip_url)
-    if os.path.isfile(zip_path):
-        zip_path_size = local_size(zip_path)
-    if not os.path.isfile(zip_path):
-        zip_path_size = 0
+    # zip_url_size = remote_size(zip_url)
+    # if os.path.isfile(zip_path):
+    #     zip_path_size = local_size(zip_path)
+    # if not os.path.isfile(zip_path):
+    #     zip_path_size = 0
 
-    if zip_url_size == zip_path_size:
-        size_flag = False
-    if zip_url_size != zip_path_size:
-        size_flag = True
+    # if zip_url_size == zip_path_size:
+    #     size_flag = False
+    # if zip_url_size != zip_path_size:
+    #     size_flag = True
+    size_flag = False
 
     if not os.path.isdir(cdl_ws):
         os.makedirs(cdl_ws)
 
-    if os.path.isfile(zip_path) and overwrite_flag or \
-        os.path.isfile(zip_path) and size_flag:
+    if os.path.isfile(zip_path) and (overwrite_flag or size_flag):
         os.remove(zip_path)
+
     if not os.path.isfile(zip_path):
         logging.info('  Download CDL files')
         logging.debug('    {}'.format(zip_url))
         logging.debug('    {}'.format(zip_path))
-        try:
-            urllib.urlretrieve(zip_url, zip_path, reporthook)
-        except IOError as e:
-            logging.error('    IOError, skipping')
-            logging.error(e)
+
+        util.ftp_download(site_url, site_folder, zip_name, zip_path)
+        # util.url_download(zip_url, zip_path)
 
     if os.path.isfile(cdl_path) and overwrite_flag:
         util.remove_file(cdl_path)
+
     if os.path.isfile(zip_path) and not os.path.isfile(cdl_path):
         logging.info('  Extracting CDL files')
         with zipfile.ZipFile(zip_path) as zf:
@@ -107,32 +106,33 @@ def arg_parse():
     return args
 
 
-def reporthook(count, block_size, total_size):
-    global start_time
-    if count == 0:
-        start_time = time.time()
-        return
-    duration = time.time() - start_time
-    progress_size = int(count * block_size)
-    speed = int(progress_size / (1024 * duration))
-    percent = int(count * block_size * 100 / total_size)
-    sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
-                    (percent, progress_size / (1024 * 1024), speed, duration))
-    sys.stdout.flush()
-
-    
-def remote_size(link):
-    site = urllib.urlopen(link)
-    meta = site.info()
-    size = meta.getheaders("Content-Length")[0]
-    return size
+# def reporthook(count, block_size, total_size):
+#     global start_time
+#     if count == 0:
+#         start_time = time.time()
+#         return
+#     duration = time.time() - start_time
+#     progress_size = int(count * block_size)
+#     speed = int(progress_size / (1024 * duration))
+#     percent = int(count * block_size * 100 / total_size)
+#     sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
+#                     (percent, progress_size / (1024 * 1024), speed, duration))
+#     sys.stdout.flush()
 
 
-def local_size(path):
-    file = open(path, "rb")
-    size = len(file.read())
-    file.close()
-    return size
+# # CGM - Need to see if this can be done through the FTP module
+# def remote_size(link):
+#     site = urllib.urlopen(link)
+#     meta = site.info()
+#     size = meta.getheaders("Content-Length")[0]
+#     return size
+
+
+# def local_size(path):
+#     file = open(path, "rb")
+#     size = len(file.read())
+#     file.close()
+#     return size
 
 
 if __name__ == '__main__':
