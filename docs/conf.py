52dcdaa4b23aa0da8ec8789efc36bd51b449b6d1
Chris Pearson <cpearson@dri.edu>
2020-03-04 09:01:22 -0800

    title text formatting edits

--- docs/conf.py
+++ docs/conf.py
@@ -225,7 +225,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'et-demands.tex', u'et-demands Documentation',
+  (master_doc, 'et-demands.tex', u'ET-Demands Documentation',
    u'Bureau of Reclamation and Desert Research Institute', 'manual'),
 ]
 
@@ -255,7 +255,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'et-demands', u'et-demands Documentation',
+    (master_doc, 'et-demands', u'ET-Demands Documentation',
      [author], 1)
 ]
 
@@ -269,7 +269,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'et-demands', u'et-demands Documentation',
+  (master_doc, 'et-demands', u'ET-Demands Documentation',
    author, 'et-demands', 'One line description of project.',
    'Miscellaneous'),
 ]
